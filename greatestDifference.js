191a2153f9b44e73ccae0c5a09b8d8869962ea4b
13DeAnda <nora.e.de.anda@gmail.com>
2016-07-08 20:17:34 -0700

    Create greatestDifference.js

---
+++ greatestDifference.js
@@ -0,0 +1,14 @@
+//Write a function that takes an array of numbers and returns the greatest difference you can get by subtracting any two of those numbers.
+var numbers = [4,64,45,34,12,9, 45, 67, 94];
+
+var min, max;
+
+	_.forEach(numbers,function(number){
+  		if(!min || min > number ){
+      		min = number
+      }
+      if(!max || max < number){
+      		max = number;
+      }
+  });
+  console.log ("the max diference", max-min);

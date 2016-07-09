6080f05557218f05ca4efd2bc857d8db1873e485
13DeAnda <nora.e.de.anda@gmail.com>
2016-07-08 20:18:51 -0700

    Update stringNonRepeated.js

--- stringNonRepeated.js
+++ stringNonRepeated.js
@@ -1,10 +1,12 @@
 //Find the first non-repeated character in a String.
 
 var name = "dazababiissso";
-		var repeated = {};
-  	_.each(name, function(chara, i){
+var repeated = {};
+
+  _.each(name, function(chara, i){
         repeated[chara] = repeated[chara]? ++repeated[chara]: 1;
-    		var shortString = name.slice(i+1, name.length);
+    	var shortString = name.slice(i+1, name.length);
+    	
         if(shortString.indexOf(chara) < 0 && repeated[chara] === 1){
         	console.log("the first non repeated character in the string is:", chara);
           return false;

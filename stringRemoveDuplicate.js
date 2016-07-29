db4dd2d345b608d1cb85d7269a0dae413a0891f2
13DeAnda <nora.e.de.anda@gmail.com>
2016-07-29 12:56:44 -0700

    Create stringRemoveDuplicate.js

---
+++ stringRemoveDuplicate.js
@@ -0,0 +1,12 @@
+var duplicateMap = {};
+var word = "AAA BBB";
+var newWord = "";
+
+_.forEach(word, function(character){
+		if(!duplicateMap[character]){
+    		newWord += character;
+        duplicateMap[character] = 1;
+    }
+});
+
+console.log(newWord);

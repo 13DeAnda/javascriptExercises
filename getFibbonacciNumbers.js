98b73bbd8d0b20bea7600a5f2f1dd661c57da577
13DeAnda <nora.e.de.anda@gmail.com>
2016-08-07 17:46:47 -0700

    Create getFibbonacciNumbers.js

---
+++ getFibbonacciNumbers.js
@@ -0,0 +1,19 @@
+/*How many Fibonacci numbers exists less than a given number n.Can you find a function in terms of n , to get the number of fibonacci number less than n. 
+Example : n = 6 
+Answer: 6 as (0, 1, 1, 2, 3, 5)*/
+
+function getFibbonacci(limit){
+	var sequence = [0,1];
+  index = 1;
+  
+  while(index < limit-1){
+  	pastValue = sequence[index];
+    pastSecondValue = sequence[index-1];
+    newValue = pastValue + pastSecondValue;
+    
+    sequence.push(newValue);
+    index++;
+  }
+   	console.log(sequence); 
+};
+getFibbonacci(6);

1a8d5e10ee401e24809adc3ed725aff8a823913a
13DeAnda <nora.e.de.anda@gmail.com>
2016-08-08 17:14:02 -0700

    Update pairSumInArray.js

--- pairSumInArray.js
+++ pairSumInArray.js
@@ -4,13 +4,15 @@ var Array = [1,2,3,5,6,7,8];
 
 var pairs = {};
 _.forEach(Array, function(number){
-	if(number <10 && number >-1){
-      	var needed = 10-number;
-        if(pairs[number]){
-        	console.log("a pair is", needed, " and ", number);
-        }
-        else{
-        	pairs[needed] = number;
-        }
-      }
-  });
+	
+  if(number <10 && number >-1){
+    var needed = 10-number;
+    
+    if(pairs[number]){
+      console.log("a pair is", needed, " and ", number);
+    }
+    else{
+      pairs[needed] = number;
+    }
+  }
+});

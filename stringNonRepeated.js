be51f6799331fdfc8bae9be77beaab05ebd74079
13DeAnda <nora.e.de.anda@gmail.com>
2016-08-08 17:19:27 -0700

    Update stringNonRepeated.js

--- stringNonRepeated.js
+++ stringNonRepeated.js
@@ -3,12 +3,12 @@
 var name = "dazababiissso";
 var repeated = {};
 
-  _.each(name, function(chara, i){
-        repeated[chara] = repeated[chara]? ++repeated[chara]: 1;
-    	var shortString = name.slice(i+1, name.length);
-    	
-        if(shortString.indexOf(chara) < 0 && repeated[chara] === 1){
-        	console.log("the first non repeated character in the string is:", chara);
-          return false;
-        }
-    });
+_.each(name, function(chara, i){
+  repeated[chara] = repeated[chara]? ++repeated[chara]: 1;
+  var shortString = name.slice(i+1, name.length);
+  	
+  if(shortString.indexOf(chara) < 0 && repeated[chara] === 1){
+    console.log("the first non repeated character in the string is:", chara);
+    return false;
+  }
+});

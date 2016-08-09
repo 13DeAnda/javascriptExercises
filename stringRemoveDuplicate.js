b93e6d2ddb5d822f12f0b896d3f2e3a2719fe1d1
13DeAnda <nora.e.de.anda@gmail.com>
2016-08-08 17:20:11 -0700

    Update stringRemoveDuplicate.js

--- stringRemoveDuplicate.js
+++ stringRemoveDuplicate.js
@@ -3,10 +3,10 @@ var word = "AAA BBB";
 var newWord = "";
 
 _.forEach(word, function(character){
-		if(!duplicateMap[character]){
-    		newWord += character;
-        duplicateMap[character] = 1;
-    }
+  if(!duplicateMap[character]){
+    newWord += character;
+    duplicateMap[character] = 1;
+  }
 });
 
 console.log(newWord);

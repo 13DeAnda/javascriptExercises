0087e077f8d7d89685e0622349920107eb071cc4
13DeAnda <nora.e.de.anda@gmail.com>
2016-08-08 17:08:30 -0700

    Update getFibbonacciNumbers.js

--- getFibbonacciNumbers.js
+++ getFibbonacciNumbers.js
@@ -3,17 +3,17 @@ Example : n = 6
 Answer: 6 as (0, 1, 1, 2, 3, 5)*/
 
 function getFibbonacci(limit){
-	var sequence = [0,1];
+  var sequence = [0,1];
   index = 1;
   
   while(index < limit-1){
-  	pastValue = sequence[index];
+    pastValue = sequence[index];
     pastSecondValue = sequence[index-1];
     newValue = pastValue + pastSecondValue;
-    
     sequence.push(newValue);
     index++;
   }
-   	console.log(sequence); 
+  console.log(sequence); 
 };
+
 getFibbonacci(6);

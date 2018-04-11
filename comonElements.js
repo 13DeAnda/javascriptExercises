1858725a61e30f82231c48e727b587b5f6adfc7f
Nora De Anda <nora.e.de.anda@gmail.com>
2018-04-11 11:59:00 -0700

    Update comonElements.js
    improvement on commonElements

--- comonElements.js
+++ comonElements.js
@@ -1,14 +1,26 @@
 //Write a function that finds the common elements in 2 int arrays
 
-var array1 = [3,4,5,6];
-var array2 = [4,6,4,7,5,90];
-var repeatedNumbers = [];
-
-_.forEach(array1, function(number){
-  if(array2.indexOf(number) > -1 && repeatedNumbers.indexOf(number) < 0){
-    repeatedNumbers.push(number);
+function getCommonElements(array1, array2){
+	var commonElementsMap = {};
+  var commonElements = [];
+  
+	for(var i = 0; i < array1.length; i++){	
+		var element1 = array1[i];
+		if(!commonElementsMap[element1]){
+  		commonElementsMap[element1] = 1;
+  	}
+	}
+  for(var j = 0; j < array2.length; j++){
+  	var element2 = array2[j];
+    if(commonElementsMap[element2] === 1){
+    	commonElements.push(element2);
+      commonElementsMap[element2] ++;
+    }
   }
-);
+  console.log("the common elements", commonElements);
+}
 
-console.log(“the repeated numbers are:”, repeatedNumbers);
+var array1 = [1,3,2,5,35,10,24];
+var array2 = [3,35,56,24,54,35];
 
+getCommonElements(array1,array2);

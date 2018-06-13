1d3d0b83e5ad3d6913fc233f285c60367351549d
Nora De Anda <nora.e.de.anda@gmail.com>
2018-06-13 17:36:31 -0500

    Update matchstr.js

--- matchstr.js
+++ matchstr.js
@@ -3,10 +3,9 @@
 "z" in a(4)b(4) --> false
 "aaaa" in a(3)b(3) ---> false
 "aab" in a(3)b(3) ---> true
-"aaba" in a(3)b(3) ---> true
+"aab" in a(3)b(3) ---> true
 
 */
-
 function getPatternMap(string){
   var map = {};
   for(var i= 0; i < string.length; i++){
@@ -22,23 +21,37 @@ function getPatternMap(string){
   return map;
 }
 
-function(string, pattern){
-  pattern = getPatternMap(pattern);
-  var patternString = "ab";
-  var stringGot = "";
-  _.forEach(string, function(character, index){
-    if(patternMap[character]){
-      	if(patternMap[character] === 1){
-          stringGot += character;
-        }
-      	patternMap[character]--;
+function matchstr(string, pattern){
+  var patternMap = getPatternMap(pattern);
+  var i = 0;
+
+  for(var pattern in patternMap){
+    var foundPattern = 0;
+    var noMoreMatches = false;
+
+    while(foundPattern < patternMap[pattern] && i < string.length && !noMoreMatches){
+      var stringToCheck = string.slice(i, i+pattern.length);
+
+      if(stringToCheck === pattern){
+        i+= pattern.length;
+        foundPattern++;
+        console.log(patternMap, stringToCheck, "matches");
+      }
+      else if(stringToCheck !== pattern && foundPattern > 0){
+        noMoreMatches = true;
       }
       else{
-      	return false;
+        return false;
       }
-  });
-  
-  if(stringGot === patternString){
+    }
+  }
+
+  if(i < string.length){
+    return false;
+  }
+  else{
     return true;
   }
 }
+
+console.log(matchstr("aab", "a(3)b(3)"));

990e642de255aa9ba6da65261d553d23abd4dbf0
nora <nora.e.de.anda@gmail.com>
2018-07-30 17:14:22 -0500

    mergeSort

--- mergeSortArrays.js
+++ mergeSortArrays.js
@@ -30,3 +30,26 @@ function sort(arr1, arr2){
 var arr1 = [5,7,8,90];
 var arr2 = [1,3,6,45,100];
 console.log(sort(arr1, arr2));
+
+
+
+function test(array1, array2) {
+  var counter1 = 0;
+  var counter2 = 0;
+  var finishedArray = [];
+
+  for (var i = 0; i < (array1.length + array2.length); i++) {
+    if ((counter1 <= array1.length) && (counter2 > array2.length || array1[counter1] < array2[counter2])) {
+      finishedArray.push(array1[counter1]);
+      counter1++;
+    } else {
+      finishedArray.push(array2[counter2]);
+      counter2++;
+    }
+  }
+
+  return finishedArray;
+}
+
+console.log(test([1,5,8], [2,3,4,6,7]));
+

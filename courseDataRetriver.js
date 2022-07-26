b374ebde0d7faf9a129e1d3395b1be78250996b2
13DeAnda <nora.e.de.anda@gmail.com>
2022-07-26 16:02:58 -0500

    removing old code

--- courseDataRetriver.js
+++ courseDataRetriver.js
@@ -31,61 +31,6 @@ Semester: Fall
 "CS111 F2014"
 **/
 
-
-
-var studentCourses = {};
-var data = ["CS111 2016Fall","CS-111 Fall 2016", "MATH 123 2015 Spring", "CS111 fall 14","CS111 F2014"];
-
-function getCourse(data){
-  var course = {};
-  course.department = data[0];
-  course.number = Number(data[1]);
-  course.semester = isNaN(data[2])? data[2] : data[3];
-  var year = isNaN(data[2])? data[3] : data[2];
-	
-  if(year.length < 4){
-    year = "20" + year;
-  }
-  course.year = Number(year);
-  
-  return course;
-};
-
-function getData(data){
-  var retrivedData = [];
-  var pastCharacter;
-  var word = "";
-	
-  _.forEach(data, function(character, index){
-    
-    if(character === " " || character === "-"){
-      retrivedData.push(word);
-      word = character;
-    }
-    else if(index === data.length-1){
-      word += character;
-      retrivedData.push(word);
-    }
-    else if(word !== "" && isNaN(pastCharacter) !== isNaN(character)){
-      retrivedData.push(word);
-      word = character;    
-    }
-    else{
-      word += character;
-    }
-    pastCharacter = character;
-  });
-  
-  return retrivedData;
-};
-
-// var dataArray = getData(data[0]);
-// var course = getCourse(dataArray);
-// console.log(dataArray, course);
-
-
-// improved version
-
 function retriveCourseData(course){
     var department = ""
     var courseNumber = ""

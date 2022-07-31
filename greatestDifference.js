41d77d1d2e051c3c668fe84023c86dc301b52f30
13DeAnda <nora.e.de.anda@gmail.com>
2022-07-31 14:43:36 -0500

    improv

--- greatestDifference.js
+++ greatestDifference.js
@@ -1,23 +1,23 @@
 //Write a function that takes an array of numbers and returns the greatest difference you can get by subtracting any two of those numbers.
-var numbers = [4,64,45,34,12,9, 45, 67, 94];
 
-function getMaxDiff(){ 
+function getMaxDiff(numbers){ 
   var min, max;
   
-  _.forEach(numbers,function(number){
+  numbers.forEach(function(number){
     if(!min || min > number ){
         min = number
      }
-     if((!max || max < number) && number !== min){
+     else if((!max || max < number) && number !== min){
         max = number;
      }
   });
   var difference = max-min;
-  if(isNaN(difference)){
-    return -1
-  }
-  else{
-    return difference;
-  }
+
+  return difference;
+
 }
+
+
+console.log(getMaxDiff([4,64,45,34,12,9, 45, 67, 94])) // 94-4 = 90
+
 

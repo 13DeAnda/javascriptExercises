d56860a060a5767f3e74ef6cd23a9e586f071b3e
13DeAnda <nora.e.de.anda@gmail.com>
2022-08-02 13:31:57 -0500

    improvement

--- makePalidromeOutOfWord.js
+++ makePalidromeOutOfWord.js
@@ -5,36 +5,44 @@ Example:
 'ttaatta' -> ' ttaaatt' 
 'abc' -> 'a' or 'b' or 'c' 
 'gggaaa' -> 'gaaag' or 'aggga' 
-
+\
 Note if there are multiple correct answers you only need to return 1 palindrome.
 */
 
 function getPalidrome(word){
   var mapWords = {};
-  var first ="";
-  var second = "";
-  var single = null;
+  var firstSequence ="";
+  var secondSequence = "";
+  var singleChar = null;
+
+    if(word.length < 2){
+      return word;
+    }
   
-  _.forEach(word, function(character){
-   if(mapWords[character]){
-     mapWords[character]++;
-     if(single === character){
-       single = null;
-     }
-     if(mapWords[character] === 2){
-       mapWords[character] = 0;
-       first += character;
-       second += character;
-     }
-    }
-    else{
-      mapWords[character] = 1;
-      single = character;
-    }
-  });	
-  if(single){
-    console.log(first+single+second);
-  }
+    for(let character of word){
+        if(mapWords[character]){
+          mapWords[character] = ++mapWords[character];
+        }
+        else{
+          mapWords[character] = 1;
+        }
+
+        if(mapWords[character] % 2){
+          singleChar = character;
+        }
+        else{
+          firstSequence = firstSequence + character;
+          secondSequence = secondSequence + character;
+          if(singleChar === character){
+            single = '';
+          }
+        }
+    }	
+    return firstSequence + singleChar + secondSequence;
 };
 
-getPalidrome("b");
+console.log("b -> b", getPalidrome("b"),  getPalidrome("b") === "b" );
+console.log("aha -> aha", getPalidrome("aha"),  getPalidrome("aha") === "aha" );
+console.log("ttaatta ->  tatatat", getPalidrome("ttaatta"),  getPalidrome("ttaatta") === "tatatat" );
+console.log("abc ->  c", getPalidrome("abc"),  getPalidrome("abc") === "c" );
+console.log("gggaaa ->  gaaga", getPalidrome("gggaaa"),  getPalidrome("gggaaa") === "gaaga" );

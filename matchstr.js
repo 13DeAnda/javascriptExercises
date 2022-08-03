5a923bc32106ed82f6aefdaa913cf0b5983b28df
13DeAnda <nora.e.de.anda@gmail.com>
2022-08-03 16:37:42 -0500

    improvement on matching strings

--- matchstr.js
+++ matchstr.js
@@ -1,68 +1,42 @@
 /*write code for this function matchstr() given
-"ab" in a(1)b(1) ---> true
-"z" in a(4)b(4) --> false
-"aaaa" in a(3)b(3) ---> false
-"aab" in a(3)b(3) ---> true
-"aab" in a(3)b(3) ---> true
-
+at least 1 char of pattern, and should not exeed the max of the pattern
 */
-function getPatternMap(string){
-  var map = {};
-  for(var i= 0; i < string.length; i++){
-    var char = string[i];
-    var leftString = string.slice(i, string.length);
+const matchstr = (string, pattern)=>{
+  let patternIndex = 0;
+  let mapPatterns = {}
 
-    var pattern = leftString.slice(0, leftString.indexOf("("));
-    var repeated = string.slice( pattern.length + 1, leftString.indexOf(")"));
-
-    i+= pattern.length + repeated.length + 1;
-    map[pattern] = parseInt(repeated);
-  }
-  return map;
-}
-
-function findMatch(string, patternMap){
-  var i = 0;
-  for(var pattern in patternMap){
-    var foundPattern = 0;
-    var noMoreMatches = false;
-
-    while(foundPattern < patternMap[pattern] && i < string.length && !noMoreMatches){
-      var stringToCheck = string.slice(i, i+pattern.length);
-
-      if(stringToCheck === pattern){
-        i+= pattern.length;
-        foundPattern++;
+  for(char of string){    
+    if(char === pattern[patternIndex]){
+      if(mapPatterns[char] < 1){
+        return false
       }
-      else if(stringToCheck !== pattern && foundPattern > 0){
-        noMoreMatches = true;
+      else if(mapPatterns[char]){
+        mapPatterns[char] = --mapPatterns[char];
       }
-      else{
-        return false;
+      else {
+        mapPatterns[char] = parseInt(pattern[patternIndex+2])-1;
       }
     }
+    else if(char === pattern[patternIndex+4]){
+      patternIndex = patternIndex+4;
+      if(mapPatterns[char] < 1){
+        return false
+      }
+      else if(mapPatterns[char]){
+        mapPatterns[char] = --mapPatterns[char];
+      }
+      else {
+        mapPatterns[char] = parseInt(pattern[patternIndex+2])-1;
+      }
+    }
+    else{
+      return false;
+    }
   }
-
-  if(i < string.length){
-    return i;
-  }
-  else{
-    return true;
-  }
-}
-
-function matchstr(string, pattern){
-  var patternMap = getPatternMap(pattern);
-  var i = 0;
-  var result = string.length -1;
-
-  while (typeof result === 'number'){
-    var slice = string.slice(i, string.length);
-    result = findMatch(slice, patternMap);
-    i = result;
-  }
-
-  return result;
-}
-
-console.log(matchstr("aabc", "a(3)b(3)"));
+  return true;
+};
+console.log(`"ab" in a(1)b(1) ---> true`, matchstr("ab", "a(1)b(1)"));
+console.log(`"z" in a(4)b(4) --> false`, matchstr("z", "a(4)b(4"));
+console.log(`"aaaa" in a(3)b(3) ---> false`, matchstr("aaaa", "a(3)b(3)"));
+console.log(`"aab" in a(3)b(3) ---> true`, matchstr("aab", "a(3)b(3)"));
+console.log(`"aabbbbb" in a(3)b(3) ---> false`, matchstr("aabbbbb", "a(3)b(3)"));
